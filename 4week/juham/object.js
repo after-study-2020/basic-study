eb37415c994ae8801079c6b4e9085fbf4741d6f8
juhyunham <judev0227@gmail.com>
2020-07-26 15:15:03 +0900

    object 4주차 과제

---
+++ 4week/juham/object.js
@@ -0,0 +1,32 @@
+function Library(arr) {
+    this.arr = arr;
+    this.find = (target) => {
+        if (arr.indexOf(target) > -1) {
+            return target
+        } else {
+            return new Error(`cannot find your target`);
+        }
+    }
+    this.add = function(target) {
+        return this.arr.push(target);
+    }
+    this.list = function () {
+        return this.arr;
+    }
+    this.delete = function (target) {
+        if (arr.indexOf(target) > -1) {
+            let myArr = this.arr;
+            let removed = this.arr.splice(arr.indexOf(target), 1)
+            return myArr;
+        }
+    }
+}
+
+const bookList = ['자바스크립트완전정복','파이썬','java의 정석']
+const bookStore = new Library(bookList);
+
+console.log(bookStore.find(`파이썬`))
+console.log(bookStore.find(`파이썬쓰~~`))
+console.log(bookStore.add(`HTML정복`)); 
+console.log(bookStore.list());
+console.log(bookStore.delete(`자바스크립트완전정복`))
