51e8af9529d8fad011b1ba976596c3c3aaf6776b
gns2862 <gns2862@naver.com>
2020-07-30 18:41:52 +0900

    4주차 방과후 문제입니다.

--- 4week/김종훈/after.js
+++ 4week/김종훈/after.js
@@ -1,8 +1,29 @@
-function Library(bookList,cb){ 
-    let result = 0;
-    for(i = 1; i < bookList.length; i++){
-        if(bookList[i] === cb){
-            return bootList[i];
+function Library(){ 
+    return{
+        find : function(name){
+            for(i = 0;i < bookList.length; i++){
+                if(bookList[i] == name){
+                    return bookList[i];
+                } 
+            }
+            return '에러';
+        },
+        add : function(name){
+            if(bookList.indexOf(name) == -1){
+                bookList.push(name);
+                return bookList;
+            }
+        },
+        list : function(){
+            return bookList;
+        },
+        delet : function(name){
+            for(i = 0;i < bookList.length; i++){
+                if(bookList[i] == name){
+                    bookList.splice(bookList.indexOf(name),1)
+                    return bookList;vi
+                }
+            }
         }
     }
 }
